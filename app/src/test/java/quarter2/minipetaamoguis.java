19bc41a66b258369fc2abcc876dc145c632d7f75
limcuando <2015-60177@mcstayuman.edu.ph>
2026-09-17 21:37:36 +0800

    minipeta2

---
+++ app/src/test/java/quarter2/minipetaamoguis.java
@@ -0,0 +1,21 @@
+package quarter2;
+
+public class myinfo {
+
+    public static void main(String[] args) {
+
+        String name = "Jhedrick Amoguis";
+        int age = 17;
+        String hobby = "Weight lifting";
+        String favoriteFood = "Fried chicken";
+        String goal = "My goal in right now is to finish senior high school.\n";
+
+        System.out.println("===== ABOUT ME =====");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
+        System.out.println("Favorite Food: " + favoriteFood);
+        System.out.println("My Goal: " + goal);
+        System.out.println("====================");
+    }
+}
