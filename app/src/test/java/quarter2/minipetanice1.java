5cb8248174f7d620d62b1361e7bc26d15d2c1810
icezcnt <francisnice1478@gmail.com>
2026-09-17 20:31:10 +0800

    added santosf profile test

---
+++ app/src/test/java/quarter2/minipetanice1.java
@@ -0,0 +1,21 @@
+package quarter2;
+
+import org.junit.Test;
+
+public class minipetanice1 {
+    @Test
+    public void printMyprofile() {
+
+        // --- 1. THE INPUT (Storing your personal details in variables) ---
+        String myname = "nice";
+        String petName = "kyadae";
+        String favFood = "karekare";
+        int myage = 17;
+
+        // --- 2. THE OUTPUT (Printing to the console) ---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myname + " and I am " + myage + " years old.");
+        System.out.println("I have wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
+}
