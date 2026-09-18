2d70979ee4354a716e1fd61b4d0c347a185186e8
Elisha <elishjintalan@gmail.com>
2026-09-18 21:59:26 +0800

    added miniPETA1

--- app/src/test/java/quarter2/minipetajintalan.java
+++ app/src/test/java/quarter2/minipetajintalan.java
@@ -1,4 +1,18 @@
 package quarter2;
 
+import org.junit.Test;
+
 public class minipetajintalan {
+    @Test
+    public void printMyProfile() {
+        String myName = "Elisha";
+        String petName = "Casper";
+        String favFood = "Pizza";
+        int myAge = 17;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would drink " + favFood + " every day");
+    }
 }
