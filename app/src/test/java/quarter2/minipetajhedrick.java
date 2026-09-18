e0d3b0be3a09849c134fa8c4afbff6f72d792b77
jayamoguis-rgb <jayamoguis@gmail.com>
2026-09-18 18:11:58 +0800

    Amoguis mini peta

--- app/src/test/java/quarter2/minipetajhedrick.java
+++ app/src/test/java/quarter2/minipetajhedrick.java
@@ -1,4 +1,23 @@
 package quarter2;
 
+import org.junit.Test;
+
 public class minipetajhedrick {
+    @Test
+    public void main() {
+
+    String name = "Jhedrick Amoguis";
+    int age = 17;
+    String hobby = "Weight lifting";
+    String favoriteFood = "Fried chicken";
+    String goal = "My goal in right now is to finish senior high school.\n";
+
+    System.out.println("===== ABOUT ME =====");
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Hobby: " + hobby);
+    System.out.println("Favorite Food: " + favoriteFood);
+    System.out.println("My Goal: " + goal);
+    System.out.println("====================");
+}
 }
