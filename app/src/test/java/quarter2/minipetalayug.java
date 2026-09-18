9cef54d1f3c310897a50b8c8d2ded27f85099c1c
vinceangelo07 <vinceangelo07rl@gmail.com>
2026-09-18 17:48:34 +0800

    Mini peta 2 test

--- app/src/test/java/quarter2/minipetalayug.java
+++ app/src/test/java/quarter2/minipetalayug.java
@@ -1,4 +1,23 @@
 package quarter2;
 
+import org.junit.Test;
+
 public class minipetalayug {
+    @Test
+    public void main() {
+
+        String name = "Vince Angelo R. Layug";
+        int age = 17;
+        String hobby = "Playing guitar";
+        String favoriteFood = "Fried chicken";
+        String goal = "My goal in right now is to graduate.\n";
+
+        System.out.println("===== ABOUT ME =====");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
+        System.out.println("Favorite Food: " + favoriteFood);
+        System.out.println("My Goal: " + goal);
+        System.out.println("====================");
+    }
 }
